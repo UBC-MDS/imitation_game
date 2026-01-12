64aa92816465e0d64bf2107c88180de6f5d67796
alxwen711 <alxwen711@gmail.com>
2026-01-12 15:43:22 -0800

    update generate_asymmetric_key function description

--- src/imitation_game/generate_asymmetric_key.py
+++ src/imitation_game/generate_asymmetric_key.py
@@ -1,7 +1,20 @@
-def generate_asymmetric_key():
+from typing import Optional
+
+def generate_asymmetric_key(public_filepath: Optional[str] = None, private_filepath: Optional[str] = None) -> tuple[str,str]:
     """
     Generates a pair of RSA keys for asymmetric encryption.
 
+    The public and private key will be printed out for the user to copy.
+    If output filepath parameters are passed then the public and private
+    keys will be saved to their respective filepaths.
+    
+    Parameters
+    ----------
+    public_filepath : str, optional (default = None)
+        Filepath to save the public key in. If no filepath is specified the public key will not be saved in a file.
+    private_filepath : str, optional (default = None)
+        Filepath to save the private key in. If no filepath is specified the private key will not be saved in a file.
+    
     Returns
     -------
     public_key : str
@@ -9,4 +22,5 @@ def generate_asymmetric_key():
     private_key : str
         The key used for decryption; must be kept secret.
     """
-    pass
+    return "",""
+
