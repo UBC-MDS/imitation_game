cc0863c1c6721db965fe496b8ed871fff59e970d
Teem KWONG <mdskwong@gmail.com>
2026-01-14 09:50:09 -0800

    Create test cases for encrypt_symmetric

--- tests/unit/test_encrypt_symmetric.py
+++ tests/unit/test_encrypt_symmetric.py
@@ -1,5 +1,11 @@
 """Tests for encrypt_symmetric function."""
+from imitation_game.encrypt_symmetric import encrypt_symmetric
 
 def test_encrypt_symmetric_basic():
     """Test file present"""
-    pass
+    key = "524"
+    plaintext = "hello world"
+    ciphertext = b'\x1e7~\xc6\x85{x\x8a\xd2=\xca'
+    expected = ciphertext
+    actual = encrypt_symmetric(plaintext, key)
+    assert actual == expected
