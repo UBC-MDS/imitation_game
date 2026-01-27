b8f17293b985c6b69d867425baae6340b4a1a2b6
Vin-dictive <vinay.valson@gmail.com>
2026-01-26 17:11:19 -0800

    Added linter fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import shutil
 # https://github.com/ttimbers/breast-cancer-predictor/blob/3.0.0/tests/conftest.py.
 
 
-@pytest.fixture(autouse=True, scope='session')
+@pytest.fixture(autouse=True, scope="session")
 def remove_testdata_directory():
     # This code will run at the end of the pytest session
     yield
